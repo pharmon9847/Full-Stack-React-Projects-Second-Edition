d65bf127b940b44a08aa9969588ec98f6443fa37
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2020-06-25 21:40:59 -0400

    changes some code

--- Chapter05/mern-social/server/server.js
+++ Chapter05/mern-social/server/server.js
@@ -4,14 +4,14 @@ import mongoose from 'mongoose'
 
 // Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect('mongodb+srv://pharmon9847:ljcx7R9iOsO8oH0b@cluster1.xhk0t.mongodb.net/mern_social?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
 mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
 app.listen(config.port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', config.port)
+if (err) {
+  console.log(err)
+}
+console.info('Server started on port %s.', config.port)
 })
